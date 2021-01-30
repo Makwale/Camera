dfd86752c2208738a25eaa8ea196f96dcb57f14c
Makwale <makwale.em@gmail.com>
2021-01-30 13:40:34 -0800

    Fist commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  imgUrl = "https://www.youtube.com/watch?v=khai2qlbOno";
+  constructor(private camera: Camera) {}
 
-  constructor() {}
+  takePhoto(){
+    const options: CameraOptions = {
+      quality: 80,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+
+      alert(base64Image);
+      this.imgUrl = base64Image;
+
+     }, (err) => {
+      // Handle error
+      alert(err.message)
+     });
+  }
 
 }
